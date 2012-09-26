8f5838dc0c92e7f6348f93fa3dadf10c99b1eebe
woxxy <woxxy@foolrulez.org>
2012-09-26 19:00:08 +0200

    fixing for composer

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@
  */
 
 \Autoloader::add_classes(array(
-	'Foolz\\Sphinxql\\Sphinxql' => __DIR__.'/classes/Sphinxql.php',
-	'Foolz\\Sphinxql\\SphinxqlConnection' => __DIR__.'/classes/SphinxqlConnection.php',
-	'Foolz\\Sphinxql\\SphinxqlExpression' => __DIR__.'/classes/SphinxqlExpression.php'
+	'Foolz\\Sphinxql\\Sphinxql' => __DIR__.'/classes/Foolz/Sphinxql/Sphinxql.php',
+	'Foolz\\Sphinxql\\SphinxqlConnection' => __DIR__.'/classes/Foolz/Sphinxql/SphinxqlConnection.php',
+	'Foolz\\Sphinxql\\SphinxqlExpression' => __DIR__.'/classes/Foolz/Sphinxql/SphinxqlExpression.php'
 ));
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 ini_set('display_errors', '1');
 
-require_once '../classes/SphinxqlExpression.php';
-require_once '../classes/SphinxqlConnection.php';
-require_once '../classes/Sphinxql.php';
+require_once '../classes/Foolz/Sphinxql/SphinxqlExpression.php';
+require_once '../classes/Foolz/Sphinxql/SphinxqlConnection.php';
+require_once '../classes/Foolz/Sphinxql/Sphinxql.php';

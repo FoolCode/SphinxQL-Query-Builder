430cbf72f0ab65b2b3ad4541bc6197c96de4fd5a
woxxy <woxxap@gmail.com>
2014-04-24 12:17:17 +0200

    Improved tests by not relying on previous methods for Sphinx content, updated CHANGELOG

--- tests/classes/SphinxQLTest.php
+++ tests/classes/SphinxQLTest.php
@@ -7,17 +7,46 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 {
     public $conn = null;
 
+    public static $data = array (
+        0 => array('id' => '10', 'gid' => '9003',
+            'title' => 'modifying the same line again', 'content' => 'because i am that lazy'),
+        1 => array('id' => '11', 'gid' => '201',
+            'title' => 'replacing value by value', 'content' => 'i have no idea who would use this directly'),
+        2 => array('id' => '12', 'gid' => '200',
+            'title' => 'simple logic', 'content' => 'inside the box there was the content'),
+        3 => array('id' => '13', 'gid' => '304',
+            'title' => 'i am getting bored', 'content' => 'with all this CONTENT'),
+        4 => array('id' => '14', 'gid' => '304',
+            'title' => 'i want a vacation', 'content' => 'the code is going to break sometime'),
+        5 => array('id' => '15', 'gid' => '304',
+            'title' => 'there\'s no hope in this class', 'content' => 'just give up'),
+        6 => array('id' => '16', 'gid' => '500',
+            'title' => 'we need to test', 'content' => 'selecting the best result in groups'),
+        7 => array('id' => '17', 'gid' => '500',
+            'title' => 'what is there to do', 'content' => 'we need to create dummy data for tests'),
+    );
+
     public function __construct()
     {
         $conn = new SphinxConnection();
         $conn->setConnectionParams('127.0.0.1', 9307);
         $this->conn = $conn;
 
-        // empty that poor db. TRUNCATE is still in beta in Sphinxsearch 2.1.1-beta
-        SphinxQL::create($this->conn)->delete()
-            ->from('rt')
-            ->where('id', 'IN', array(1, 10, 11, 12, 13, 14, 15, 16, 17))
-            ->execute();
+        SphinxQL::create($this->conn)->getConnection()->query('TRUNCATE RTINDEX rt');
+    }
+
+    public function refill() {
+        SphinxQL::create($this->conn)->getConnection()->query('TRUNCATE RTINDEX rt');
+
+        $sq = SphinxQL::create($this->conn)->insert()
+            ->into('rt')
+            ->columns('id', 'gid', 'title', 'content');
+
+        foreach (static::$data as $row) {
+            $sq->values($row['id'], $row['gid'], $row['title'], $row['content']);
+        }
+
+        $sq->execute();
     }
 
     public function testExpr()
@@ -278,6 +307,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
      */
     public function testWhere()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->where('gid', 'BETWEEN', array(300, 400))
@@ -318,6 +349,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
      */
     public function testMatch()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->match('content', 'content')
@@ -350,6 +383,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 
     public function testOption()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->match('content', 'content')
@@ -361,6 +396,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 
     public function testGroupBy()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select(SphinxQL::expr('count(*)'))
             ->from('rt')
             ->groupBy('gid')
@@ -372,6 +409,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 
     public function testOrderBy()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->orderBy('id', 'desc')
@@ -389,6 +428,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 
     public function testWithinGroupOrderBy()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->where('gid', 500)
@@ -410,6 +451,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 
     public function testOffset()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->offset(4)
@@ -420,6 +463,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
 
     public function testLimit()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)->select()
             ->from('rt')
             ->limit(3)
@@ -441,6 +486,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
      */
     public function testDelete()
     {
+        $this->refill();
+
         SphinxQL::create($this->conn)->delete()
             ->from('rt')
             ->where('id', 'IN', array(10, 11, 12))
@@ -462,6 +509,8 @@ class SphinxQLTest extends PHPUnit_Framework_TestCase
      */
     public function testQueue()
     {
+        $this->refill();
+
         $result = SphinxQL::create($this->conn)
             ->select()
             ->from('rt')

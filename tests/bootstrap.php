2af1574c68726cdcd27a811190980daf7f8f7811
Hung Tran <hung@0x0.ninja>
2017-11-07 06:52:19 -0600

    update backwards compat fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,9 @@
 // PHPUnit Backwards Compatability Fix
 if (!class_exists('\PHPUnit\Framework\TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+if (class_exists('\PHPUnit\Framework\Error\Warning')) {
     class_alias('\PHPUnit_Framework_Error_Warning', '\PHPUnit\Framework\Error\Warning');
 }
 

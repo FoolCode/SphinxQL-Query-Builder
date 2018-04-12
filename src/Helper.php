8cc648f8fb1d68e3a20189a285c5e2faa5b8acc6
Vladimir <v.yu.chebotarev@mail.ru>
2018-04-12 17:12:33 +0300

    make Helper constructor public (#133)

--- src/Helper.php
+++ src/Helper.php
@@ -15,7 +15,7 @@ class Helper
      */
     protected $connection;
 
-    protected function __construct(ConnectionInterface $connection)
+    public function __construct(ConnectionInterface $connection)
     {
         $this->connection = $connection;
     }

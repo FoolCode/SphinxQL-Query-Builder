af8751c3822003c4dd378d401fe805fd6937119e
Hung Tran <hung@0x0.ninja>
2018-04-14 11:18:19 -0500

    fix sphinx3

--- tests/run.sh
+++ tests/run.sh
@@ -2,9 +2,13 @@
 
 case $SEARCH_BUILD in
   SPHINX2)
-    $HOME/search/usr/bin/searchd -c sphinx.conf
+    $WORK=$HOME/search
+    gcc -shared -o data/test_udf.so test_udf.c
+    $WORK/usr/bin/searchd -c sphinx.conf
     ;;
   SPHINX3)
+    $WORK=$HOME/search/sphinx-3.0.3
+    gcc -shared -o data/test_udf.so $HOME/src/udfexample.c
     $HOME/search/sphinx-3.0.3/bin/searchd -c sphinx.conf
     ;;
   MANTICORE)

1c3307db17fca8d936afcfbe5b155a5e3b61ba9a
Hung Tran <hung@0x0.ninja>
2018-04-14 12:10:22 -0500

    fix path

--- tests/run.sh
+++ tests/run.sh
@@ -2,17 +2,17 @@
 
 case $SEARCH_BUILD in
   SPHINX2)
-    $WORK=$HOME/search
+    WORK=$HOME/search
     gcc -shared -o data/test_udf.so test_udf.c
     $WORK/usr/bin/searchd -c sphinx.conf
     ;;
   SPHINX3)
-    $WORK=$HOME/search/sphinx-3.0.3
+    WORK=$HOME/search/sphinx-3.0.3
     gcc -shared -o data/test_udf.so $HOME/src/udfexample.c
     $HOME/search/sphinx-3.0.3/bin/searchd -c sphinx.conf
     ;;
   MANTICORE)
-    $WORK=$HOME/search
-    searchd -c sphinx.conf
+    WORK=$HOME/search
+    $WORK/usr/bin/searchd -c sphinx.conf
     ;;
 esac
